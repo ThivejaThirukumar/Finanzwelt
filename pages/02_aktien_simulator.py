bfbe120b8b2095165789e9162456e7a565a26f45
ThivejaThirukumar <thiruth1@students.zhaw.ch>
2026-06-30 10:44:04 +0200

    refactor: Optimize stock price simulation loop and initialize course list

--- pages/02_aktien_simulator.py
+++ pages/02_aktien_simulator.py
@@ -3,12 +3,18 @@ import random
 import plotly.express as px 
 
 startkurs = 100.0 
-kurse = []
-kurse.append(startkurs)
+rendite = -100  # Startwert, garantiert < 20, damit die Schleife überhaupt startet
 
-for tag in range(251):
-    veraenderung = random.gauss(0.0005, 0.015)
-    neuer_kurs = kurse[-1] * (1 +veraenderung)
-    kurse.append(neuer_kurs)
+while rendite <= 20:
+    kurse = [startkurs]
+    for tag in range(251):
+        veraenderung = random.gauss(0.0005, 0.015)
+        neuer_kurs = kurse[-1] * (1 + veraenderung)
+        kurse.append(neuer_kurs)
+    
+    hoechstkurs = max(kurse)
+    tiefstkurs = min(kurse)
+    schlusskurs = kurse[-1]
+    rendite = (schlusskurs - startkurs) / startkurs * 100
 
-print(kurse)
+print(rendite)
