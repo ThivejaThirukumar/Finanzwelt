65d88c49e078bc7a06451456997a252aa1ae6a3b
ThivejaThirukumar <thiruth1@students.zhaw.ch>
2026-06-30 11:00:04 +0200

    feat: Enhance stock simulator UI with interactive sliders and metrics display

--- pages/02_aktien_simulator.py
+++ pages/02_aktien_simulator.py
@@ -17,4 +17,22 @@ while rendite <= 20:
     schlusskurs = kurse[-1]
     rendite = (schlusskurs - startkurs) / startkurs * 100
 
-print(rendite)
+st.title("📊 Aktien-Simulator")
+st.slider("Startkurs(CHF)", 10, 500, 100)
+st.slider("Volatilität", 0.005, 0.05, 0.015)
+random.gauss(0.005, vol)
+
+col1, col2, col3 = st.columns(3)
+col1.metric("startkurs", f"CHF {startkurs;.0f}")
+col2.metric("Schlusskurs", f"CHF {startkurs:.0f}")
+col3.metric("Rendite", f"{rendite:.1f}%")
+
+tage = list(range(252))
+fig = px.line(x = tage, y = kursem title = "Kursverlauf 252 Handelstage")
+if > 0:
+    color= "green"
+else: 
+    color = "red"
+
+st.plotly_chart(fig, use_container_width=True)
+st.expander("💡 Was ist Volatilität?") 
